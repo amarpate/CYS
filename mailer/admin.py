7582727c884a0e6bca9a4247d8f0b258511ce5d7
devstein <devstein@seas.upenn.edu>
2014-06-11 18:00:36 -0700

    Trimmed .model
    Added get_emails to Recipient Manager

--- mailer/admin.py
+++ mailer/admin.py
@@ -3,5 +3,5 @@ from .models import Recipient
 # Register your models here.
 class RecipientAdmin(admin.ModelAdmin):
 	list_display = ('user_email', 'first_name', 'last_name', 'created')
-
+	search_fields = ['user_email', 'first_name', 'last_name']
 admin.site.register(Recipient, RecipientAdmin)
--- mailer/models.py
+++ mailer/models.py
@@ -9,14 +9,18 @@ from django.dispatch import receiver
 # Create your models here.
 
 SENDER = "Devstein@seas.upenn.edu"
-CONTACT_LIST = [] 
 EMAIL_LIST = [] 
 
 class RecipientManager(models.Manager):
 	"""
 	A model to control the mail list
 	"""
-	
+	def get_emails(self):
+		email_list = [] 
+		for email in self.all():
+			email_list.append(str(email.user_email))
+		return email_list
+
 	def mailAll(self):
 		pass
 
@@ -27,34 +31,10 @@ class Recipient(models.Model):
 	user_email = models.EmailField(max_length=254, validators=[validate_email_with_name] )
 	created = models.DateTimeField(auto_now_add=True, db_index=True)
 	
-	def subscribe(self):
-		EMAIL_LIST.append(self.user_email.lower())
-		CONTACT_LIST.append([self.first_name.lower(), self.last_name.lower()])
-
-	def save(self, *args, **kwargs):
-		#Have to create check to see if recipient already exists 
-		if (self.user_email.lower() not in EMAIL_LIST):
-			#Place to do something with recipients
-			self.subscribe()
-			#Add success comfirmation 
-			return super(Recipient, self).save(*args, **kwargs)
-		else:
-			pass
-		#Add else statement with error message
-
-
-
-	@receiver(pre_delete, sender=RecipientManager)
-	def unsubscribe(sender, instance, **kwargs):
-		#Check to see if email exists
-		if (self.user_email.lower() in EMAIL_LIST):
-			EMAIL_LIST.remove(self.user_email.lower())
-			CONTACT_LIST.remove([self.first_name.lower(), self.last_name.lower()])
-			#Add successfully removed from email list   
-		else:
-			pass
-		#Add else statement for error message 
 
 	def __unicode__(self):
 		return self.user_email
+	
+	recipients = RecipientManager() 
+
 	
