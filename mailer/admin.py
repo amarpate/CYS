3fc2188febbdbee5efda904f995f1a5b947306e7
devstein <devstein@seas.upenn.edu>
2014-06-17 07:05:12 -0700

    Removed duplicated mailer directory

--- mailer/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Recipient
-# Register your models here.
-class RecipientAdmin(admin.ModelAdmin):
-	list_display = ('user_email', 'first_name', 'last_name', 'created')
-	search_fields = ['user_email', 'first_name', 'last_name']
-admin.site.register(Recipient, RecipientAdmin)
--- mailer/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-from django.conf import settings
-from django.core.mail import send_mail
-# Create your views here.
